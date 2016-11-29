cb9b9678a0cb1ffdfb3be8cc10b38847095fb4a4
Randy Yang <randy_yang@premierinc.com>
2016-11-29 14:24:28 -0500

    Initial Commit

---
+++ backend/src/main/java/io/gitlab/randyyaj/HelloController.java
@@ -0,0 +1,13 @@
+package io.gitlab.randyyaj;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/api/hello")
+    public String hello() {
+        return "hello";
+    }
+}
